97c4e7f853b7aa881b7d44c0e5c09843f8594984
benburton <benjamin.joseph.burton@gmail.com>
2018-04-06 10:40:38 -0400

    Add function to delete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 const myConfigValue = process.env.foo;
 
-const aFunctionToDelete = () => {
-    console.log(`tear me down! I'm pointless!`);
-}
-
 const printConfigValue = () => {
     console.log(myConfigValue);
 }
